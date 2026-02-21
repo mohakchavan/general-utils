4d8c669b351356c4088528bc68cf9fb1ac8c39bd
Mohak Chavan <262987772+mohakchavan@users.noreply.github.com>
2026-02-21 18:12:22 +0530

    Initial Commit.

---
+++ src/main/java/io/github/mohakchavan/App.java
@@ -0,0 +1,13 @@
+package io.github.mohakchavan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
