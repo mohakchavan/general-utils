7a8ea6009d724c8ecdfd7f6c1a793a52e2c1f614
Mohak Chavan <262987772+mohakchavan@users.noreply.github.com>
2026-05-02 15:45:07 +0530

    Changes in the workflow file.

---
+++ .github/scripts/delete-cache.sh
@@ -0,0 +1,22 @@
+echo "Executing delete-cache script..."
+
+print_branch() {
+  echo "Inside print_branch"
+  echo "Received $1"
+}
+
+if [ "$#" -eq 0 ]; then
+  echo "Command line arguments required."
+  exit 1
+
+elif [ -z "$1" ]; then
+    echo "GitHub token is required."
+    exit 1
+
+elif [ -z "$2" ]; then
+    echo "Branch name is required."
+    exit 1
+
+fi
+
+print_branch "$2"
