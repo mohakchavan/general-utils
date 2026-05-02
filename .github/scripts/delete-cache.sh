0e3ece113ecbfa7485bd44963c0a0d8b90878f27
Mohak Chavan <262987772+mohakchavan@users.noreply.github.com>
2026-05-02 16:42:43 +0530

    Changes in the workflow file.

--- .github/scripts/delete-cache.sh
+++ .github/scripts/delete-cache.sh
@@ -1,10 +1,11 @@
-echo "Executing delete-cache.sh $0 script..."
+echo "Executing delete-cache.sh script..."
 
 print_branch() {
   echo "Inside print_branch"
   echo "Received $1"
 
-
+  curl --location --request GET --header "Authorization: Bearer $token" \
+    "https://api.github.com/repos/$repo/actions/caches" --data "ref=$1" --verbose
 }
 
 if [ "$#" -eq 0 ]; then
@@ -12,17 +13,22 @@ if [ "$#" -eq 0 ]; then
   exit 1
 
 elif [ -z "$1" ]; then
-    echo "GitHub token is required."
+    echo "GitHub repository is required."
     exit 1
 
 elif [ -z "$2" ]; then
+    echo "GitHub token is required."
+    exit 1
+
+elif [ -z "$3" ]; then
     echo "Branch name is required."
     exit 1
 
 fi
 
+repo="$1"
+shift 1
 token="$1"
-
 shift 1
 
 for br in "$@"; do
