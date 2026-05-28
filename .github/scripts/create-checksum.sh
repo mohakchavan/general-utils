1c8433cf12702c3346d8f661c4b2951bf8d432db
Mohak Chavan <262987772+mohakchavan@users.noreply.github.com>
2026-05-28 19:35:10 +0530

    Changes made in workflow file.

--- .github/scripts/create-checksum.sh
+++ .github/scripts/create-checksum.sh
@@ -3,8 +3,7 @@ echo "Running create-checksum.sh script..."
 checksums() {
   echo "Inside checksums"
 
-  for file in "$1"; do
-    echo
+  for file in $1; do
     echo "For $file file..."
     md5sum $file | cut --delimiter " " --fields 1 > $file.md5
     sha1sum $file | cut --delimiter " " --fields 1 > $file.sha1
