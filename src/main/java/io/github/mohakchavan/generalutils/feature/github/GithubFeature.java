b5b90901de435c928b57ec44a0f66d190cfff72e
Mohak Chavan <262987772+mohakchavan@users.noreply.github.com>
2026-05-30 17:57:16 +0530

    - Conversion to base64 option added for files in github feature. - Some text changes made.

--- src/main/java/io/github/mohakchavan/generalutils/feature/github/GithubFeature.java
+++ src/main/java/io/github/mohakchavan/generalutils/feature/github/GithubFeature.java
@@ -95,14 +95,14 @@ public class GithubFeature {
     }
 
     private String acquireSecret() {
-        ConsoleInputOutput.write("\nEnter Secret Value:");
+        ConsoleInputOutput.write("\nEnter Secret Value (single line only):");
         String secret = scanner.nextLine();
         secret = secret.trim();
         return secret;
     }
 
     private String acquireFileSecret() {
-        ConsoleInputOutput.write("\nEnter file-path including the file to be encrypted:");
+        ConsoleInputOutput.write("\nEnter full file-path including the file to be encrypted:");
         String filePath = scanner.nextLine();
         if (helper.isStringEmpty(filePath)) {
             ConsoleInputOutput.write("Path of the file cannot be null or empty.");
@@ -126,7 +126,19 @@ public class GithubFeature {
             throw new ExitException();
         }
         byte[] fileBytes = readBytesFromFile(file);
-        return Base64.getEncoder().encodeToString(fileBytes);
+        ConsoleInputOutput.write("File reading completed.");
+
+        return convertContentsInputBased(fileBytes);
+    }
+
+    private String convertContentsInputBased(byte[] fileBytes) {
+        ConsoleInputOutput.write("\nConvert file contents to Base64, if not already converted ? " +
+                "If selected y, remember to first decode from Base64 before using." +
+                "\nDefault y, (y/N):");
+        String input = scanner.nextLine();
+        boolean convert = helper.isStringEmpty(input) ||
+                (!"no".equalsIgnoreCase(input) && !"n".equalsIgnoreCase(input));
+        return convert ? Base64.getEncoder().encodeToString(fileBytes) : new String(fileBytes);
     }
 
     private byte[] readBytesFromFile(File fileToRead) {
